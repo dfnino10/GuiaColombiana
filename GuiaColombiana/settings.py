9a8cf3f40910963b4dccc052051a0c28990e1b44
Camilo Ortiz <ca.ortiz1633@uniandes.edu.co>
2019-08-20 15:14:43 -0500

    Requirements added

--- GuiaColombiana/settings.py
+++ GuiaColombiana/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,6 +75,7 @@ WSGI_APPLICATION = 'GuiaColombiana.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES['default'] = dj_database_url.config()
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql',
