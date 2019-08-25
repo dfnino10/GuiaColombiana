17e97055746d0ab0057afbb6b9ce4603aeccbcc9
Camilo Ortiz <ca.ortiz1633@uniandes.edu.co>
2019-08-25 17:54:29 -0500

    Login started

--- GuiaColombiana/Apps/AdministracionGuia/views.py
+++ GuiaColombiana/Apps/AdministracionGuia/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import authenticate, login
+from django.core.serializers import json
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'POST':
+        jsonUser = json.loads(request.body)
+        username = jsonUser['username']
+        password = jsonUser['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            message = 'Ok'
+        else:
+            message = 'Not ok'
+
+        return JsonResponse({'message': 'message'})
+
+
