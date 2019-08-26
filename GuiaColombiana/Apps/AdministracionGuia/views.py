33b0c781f7c5158cabca5141189c8fe854f88e8e
Camilo Ortiz <ca.ortiz1633@uniandes.edu.co>
2019-08-26 02:19:39 -0500

    Return guides revoked

--- GuiaColombiana/Apps/AdministracionGuia/views.py
+++ GuiaColombiana/Apps/AdministracionGuia/views.py
@@ -73,7 +73,6 @@ def login_method(request):
         if user is not None:
             login(request, user)
             message = 'Bienvenido ' + username
-            return view_all_guides(request)
         else:
             message = 'Usuario o contrasenia incorrectos.'
 
